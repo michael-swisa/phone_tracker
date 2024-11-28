e949a9559d3fa2a45d9c588589f3169b5a63a6c6
Michael Swisa <ms0504109182@gmail.com>
2024-11-28 17:52:06 +0200

    add route /api/bluetooth_connections

--- routes.py
+++ routes.py
@@ -31,6 +31,7 @@ def get_bluetooth_connections():
     tracker = PhoneTracker(neo4j_driver)
     bluetooth_connections = tracker.count_bluetooth_connections()
 
+
     return jsonify({'result': bluetooth_connections}), 200
 
 
